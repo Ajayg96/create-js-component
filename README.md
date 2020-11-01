462dd270d054dd98cb136fa7e2aaa6be6295c763
Ajay G <ajayg9626@gmail.com>
2020-11-01 16:45:18 +0530

    support for react and react-native, v1.0.2 publish

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Create-Js-File
 
-> A command line tool to create file
+> A command line tool to create basic react/react-native component
 
 [![NPM](https://img.shields.io/npm/v/create-js-file.svg)](https://www.npmjs.com/package/create-js-file) [![JavaScript Style Guide](https://img.shields.io/badge/code_style-standard-brightgreen.svg)](https://standardjs.com)
 
@@ -17,7 +17,7 @@ yarn global add create-js-file
 ## Usage
 
 ```js
-create-js-file hello.js
+create-js-file Hello.js
 ```
 
 ## License
--- src/index.js
+++ src/index.js
@@ -1,15 +1,111 @@
 #!/usr/bin/env node
 
 const fs = require("fs");
+const inquirer = require("inquirer");
 
-function createFile() {
-  const filename = process.argv[2];
-  const cwd = process.cwd();
-  fs.writeFileSync(
-    `${cwd}/${filename}`,
-    "export default function helloWold() { console.log('Hello World') }"
+async function createFile() {
+  const fileName = process.argv[2];
+  if (fileName) {
+    const { framework, component } = await inquirer.prompt([
+      {
+        type: "list",
+        message: "Pick the framework you're using:",
+        name: "framework",
+        choices: ["React", "React-Native"],
+      },
+      {
+        type: "list",
+        name: "component",
+        message: "Type of component ?",
+        choices: ["Functional Component", "Class Component"],
+      },
+    ]);
+    let data = componentContent(component, fileName, framework);
+    const cwd = process.cwd();
+    if (fileName.includes(".js") || fileName.includes(".jsx")) {
+      fs.writeFileSync(`${cwd}/${fileName}`, data);
+      console.log(`${component} ${fileName} created at ${cwd}`);
+    } else {
+      console.log("Extension should be either .js or .jsx");
+    }
+  } else {
+    console.log("Please enter filename");
+  }
+}
+
+function componentContent(type, fileName, framework) {
+  let componentName = "";
+  let component = "";
+  if (fileName.includes(".js")) {
+    componentName = fileName.slice(0, -3);
+  }
+  if (fileName.includes(".jsx")) {
+    componentName = fileName.slice(0, -4);
+  }
+  if (framework === "React") {
+    if (type === "Functional Component") {
+      component = `
+import React from 'react';
+
+const ${componentName} = () => {
+  return (
+    <div>${componentName} component</div>
   );
-  console.log(`${filename} created at ${cwd}`);
+}
+
+export default ${componentName};
+    `;
+    } else {
+      component = `
+import React from 'react';
+
+class ${componentName} extends Component {
+  render() {
+    return (
+      <div>${componentName} component</div>
+    );
+  }
+}
+
+export default ${componentName};
+    `;
+    }
+  } else {
+    if (type === "Functional Component") {
+      component = `
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const ${componentName} = () => {
+  return (
+    <View>
+      <Text>${componentName} component</Text>
+    </View>
+  );
+}
+
+export default ${componentName};
+    `;
+    } else {
+      component = `
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+class ${componentName} extends Component {
+  render() {
+    return (
+      <View>
+        <Text> ${componentName} component </Text>
+      </View>
+    );
+  }
+}
+
+export default ${componentName};
+    `;
+    }
+  }
+  return component;
 }
 
 createFile();
